3b92cb4602cd1e1de85e34e57d69a19fcbedd574
tremost5 <tremostayang5@gmail.com>
2026-02-25 05:47:38 +0700

    fix: render xml declaration safely in sitemap blade

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+@php echo '<?xml version="1.0" encoding="UTF-8"?>'; @endphp
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach($urls as $url)
     <url>
@@ -10,4 +10,3 @@
     </url>
 @endforeach
 </urlset>
-
